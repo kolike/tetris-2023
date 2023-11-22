335c9f3f3e10e5b41c8f5d1494ebfed72cc1cb24
Nikolay <kolik_2604@mail.ru>
2023-11-22 17:09:33 +0300

    fix fix fix

--- app/components/Field.tsx
+++ app/components/Field.tsx
@@ -24,59 +24,74 @@ const Field = () => {
   const { fieldSettings } = useFieldSettings();
   const { cellSize, linesCount, columnsCount } = fieldSettings;
   const [cellsState, setСellsState] = useState<CellBoolArr>([]);
-  const [coordX, setCoordX] = useState(0);
-  const [coordY, setCoordY] = useState(0);
-
-  const field = useMemo(() => {
-    const fieldArr: CellBoolArr = [];
-    for (let i = 0; i < linesCount; i++) {
-      fieldArr[i] = new Array();
-    }
-    for (let i = 0; i < linesCount; i++) {
-      for (let j = 0; j < columnsCount; j++) {
-        fieldArr[i][j] = false;
-      }
-    }
-
-    return fieldArr;
-  }, [linesCount, columnsCount, coordX, coordY]);
-
-  const onKeydown = (e: KeyboardEvent) => {
-    switch (e.key) {
-      case "ArrowDown":
-        if (coordX >= 0 && coordX < linesCount - 1) {
-          setCoordX((prev) => prev + 1);
-        }
-        break;
-      case "ArrowUp":
-        if (coordX > 0 && coordX <= linesCount) {
-          setCoordX((prev) => prev - 1);
-        }
-        break;
-      case "ArrowLeft":
-        if (coordY > 0 && coordY <= columnsCount) {
-          setCoordY((prev) => prev - 1);
-        }
-        break;
-      case "ArrowRight":
-        if (coordY >= 0 && coordY < columnsCount - 1) {
-          setCoordY((prev) => prev + 1);
-        }
-        break;
-    }
-  };
 
   useEffect(() => {
-    const copyArr = [...field];
-    copyArr[coordX][coordY] = true;
-    setСellsState(copyArr);
+    setСellsState(createFild(0, 0));
+  }, [fieldSettings]);
+
+  useEffect(() => {
+    const onKeydown = (e: KeyboardEvent) => {
+      let [i, j]: any = find();
+      switch (e.key) {
+        case "ArrowDown":
+          if (i >= 0 && i < linesCount - 1) {
+            i++;
+          }
+          break;
+        case "ArrowUp":
+          if (i > 0 && i <= linesCount) {
+            i--;
+          }
+          break;
+        case "ArrowLeft":
+          if (j > 0 && j <= columnsCount) {
+            j--;
+          }
+          break;
+        case "ArrowRight":
+          if (j >= 0 && j < columnsCount - 1) {
+            j++;
+          }
+          break;
+      }
+      setСellsState(createFild(i, j));
+    };
 
     document.addEventListener("keydown", onKeydown);
 
     return () => {
       document.removeEventListener("keydown", onKeydown);
     };
-  }, [coordX, coordY]);
+  }, [cellsState]);
+
+  function createFild(x: any, y: any) {
+    const fieldArr: CellBoolArr = [];
+
+    for (let i = 0; i < linesCount; i++) {
+      fieldArr[i] = new Array();
+    }
+
+    for (let i = 0; i < linesCount; i++) {
+      for (let j = 0; j < columnsCount; j++) {
+        fieldArr[i][j] = false;
+      }
+    }
+    fieldArr[x][y] = true;
+
+    return fieldArr;
+  }
+
+  function find() {
+    for (let i = 0; i < cellsState.length; i++) {
+      if (cellsState[i].includes(true)) {
+        for (let j = 0; j < cellsState[i].length; j++) {
+          if (cellsState[i][j]) {
+            return [i, j];
+          }
+        }
+      }
+    }
+  }
 
   return (
     <FieldSpace
